50db894bdd8eddc2413a77302fa9045b0172705c
chihabMg <chihab.mg.me@gmail.com>
2023-05-23 17:55:21 +0100

    removed t3 stack

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,6 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {},
+  "cssModules": {
+    "/home/user/Documents/projects/portfolio-v4/src/app/page": []
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { demos } from "#/lib/demos";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 bg-gray-400">
       hi
     </div>
   );
